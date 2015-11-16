7ea8021be7a565305a399277218daca824c2a76a
helloaihao <543160349@qq.com>
2015-11-16 18:43:03 +0800

    update

---
+++ app/script/News/News.js
@@ -0,0 +1,22 @@
+﻿
+var Newslist = function () {
+    var self = this;
+    var urls = common.gServerUrl + "/API/Common/News/1012";
+    self.news = ko.observable({});
+    self.getNews = function () {
+        $.ajax({
+            url: urls,
+            type: 'GET',
+            success: function (responseText) {
+                var obj = JSON.parse(responseText);
+                self.news(obj);
+            }
+        });
+    };
+
+    $(document).ready(function () {
+        self.getNews();
+    })
+}
+
+ko.applyBindings(Newslist);
---
+++ app/script/index.js
@@ -0,0 +1,34 @@
+﻿var TeacherList = function () {
+    var self = this;
+    var geturl = common.gServerUrl + "API/Teacher/GetIndexTeachers?count=8";
+    self.list = ko.observableArray([]);
+    self.getTeacher = function () {
+        $.ajax({
+            url: geturl,
+            type: "GET",
+            success: function (data) {
+                var arr = JSON.parse(data);
+                self.list(arr);
+                var lkSlideset = document.getElementById('lk-slideset');
+                var slideset = UIkit.slideset(lkSlideset, { small: 1, medium: 2, large: 4 });
+            }
+        });
+    }();
+
+    var carouselurl = common.gServerUrl + "Common/Slider/GetList";
+    self.CarouselCount = ko.observableArray([]);
+    self.getCarousel = function () {
+        $.ajax({
+            url: carouselurl,
+            type: "get",
+            success: function (data) {
+                var dts = JSON.parse(data);
+                self.CarouselCount(dts);
+                var lkSlideshow = document.getElementById('lk-slideshow');
+                var slideshow = UIkit.slideshow(lkSlideshow, {autoplay:true});
+            }
+        });
+    }();
+    
+}
+ko.applyBindings(TeacherList);
